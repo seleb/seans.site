b073fc9bb4b59381d6fa70e1dd31b453999c2600
Sean LeBlanc <sean.s.leblanc@gmail.com>
2017-05-01 21:49:16 -0400

    initial commit

---
+++ js/db.js
@@ -0,0 +1,433 @@
+var db = [
+	
+	{
+		title: "Duck Duck Plays the Blues",
+		image: "Duck Duck Plays the Blues",
+		association: "Personal Project",
+		tags:["software","music","PICO-8"],
+		date: "March 2017",
+		tagline: "Play the blues like a duck!",
+		description: "A little PICO-8 music toy. Only one duck this time around, but now they've got a sax!",
+		links:[
+			["itch","https://seansleblanc.itch.io/duck-duck-plays-the-blues"]
+		]
+	},{
+		title: "holt",
+		image: "holt",
+		association: "#utopiajam",
+		tags:["software","games","bitsy"],
+		date: "February 2017",
+		tagline: "a bitsy utopia adventure",
+		description: "A short adventure game made with Bitsy for Utopia Jam.",
+		links:[
+			["itch","https://seansleblanc.itch.io/holt"]
+		]
+	},{
+		title: "Cover Your EP",
+		image: "Cover Your EP",
+		association: "#EPJam5",
+		tags:["software","music"],
+		date: "January 2017",
+		tagline: "audio not meant for listening",
+		description: "A generative audio EP made using a variety of different techniques.",
+		links:[
+			["itch","https://seansleblanc.itch.io/cover-your-ep"],
+			["genericlink","https://soundcloud.com/seansleblanc/sets/cover-your-ep"]
+		]
+	},
+	{
+		title: "Tweetcarts - 2016",
+		image: "Tweetcarts - 2016",
+		association: "#tweetjam",
+		tags:["software","PICO-8"],
+		date: "June - December 2016",
+		tagline: "PICO-8 in 140 characters or less",
+		description: "A set of 50 animations, each with source code short enough to fit in a tweet.",
+		links:[
+			["itch","https://seansleblanc.itch.io/tweetcarts-2016"]
+		]
+	},
+	{
+		title: "gui-butler",
+		image: "gui-butler",
+		association: "Personal Project",
+		tags:["software","tool"],
+		date: "December 2016",
+		tagline: "an itch.io butler wrapper for lazybones like me",
+		description: "A GUI wrapper for the butler CLI which provides access to some of the features not yet available in the official itch app or web interface.",
+		links:[
+			["itch","https://seansleblanc.itch.io/gui-butler"],
+			["git","https://github.com/seleb/gui-butler"]
+		]
+	},
+	{
+		title: "BBwtBBB",
+		image: "Bouncing Balls with the Boing Boing Boys",
+		association: "Ludum Dare 37",
+		tags:["software","PixiJS"],
+		date: "December 2016",
+		tagline: "Bouncy Battles for 2-4 Buddies",
+		description: "Bouncing Balls with the Boing Boing Boys: Local multiplayer deathmatch for 2-4 players. Made with Pixi.js.",
+		links:[
+			["itch","https://sweetheartsquad.itch.io/bouncing-balls-with-the-boing-boing-boys"],
+			["git","https://github.com/sweetheartsquad/LDJAM37"],
+			["genericlink","http://ludumdare.com/compo/ludum-dare-37/?action=preview&uid=50235"]
+		]
+	},
+	{
+		title: "STAIRS",
+		image: "STAIRS",
+		association: "Personal Project",
+		tags:["software","PICO-8"],
+		date: "December 2016",
+		tagline: "&#9776;&#9777;&#9779;&#9783;",
+		description: "PICO-8 falling hazards.",
+		links:[
+			["itch","https://seansleblanc.itch.io/stairs"],
+			["itch","https://seansleblanc.itch.io/stairs2"],
+			["itch","https://seansleblanc.itch.io/stairs3"]
+		]
+	},
+	{
+		title: "NaBoMaMo2016",
+		image: "NaBoMaMo2016",
+		association: "#NaBoMaMo",
+		tags:["software"],
+		date: "November 2016",
+		tagline: "Some bots I made.",
+		description: "A set of six bots written during a 30-day bot-making challenge, primarily made with cheapbotsdonequick.com.",
+		links:[
+			["tumblr", "http://blog.seans.site/post/153916226291/nabomamo-2016-wrap-up"],
+			["twitter", "https://twitter.com/SeanSLeBlanc/lists/my-bots"],
+			["git", "https://github.com/seleb/NaBoMaMo2016"]
+		]
+	},
+	{
+		title: "commute",
+		image: "commute",
+		association: "#YOUjam",
+		tags:["software","games","PICO-8"],
+		date: "November 2016",
+		tagline: "try to get some rest<br>don't miss your stop",
+		description: "A tiny autobiography made in PICO-8.",
+		links:[
+			["itch", "https://seansleblanc.itch.io/commute"],
+			["git", "https://github.com/seleb/YOUjam2016"]
+		]
+	},
+	{
+		title: "Tackle Tourney TURBO",
+		image: "Tackle Tourney TURBO",
+		association: "#FishingJam 2",
+		tags:["software","games","PixiJS"],
+		date: "October 2016",
+		tagline: "Fishing Free-For-All For 2-4 Players",
+		description: "Local multiplayer game for 2-4 players with plenty of fake physics. Made with Pixi.js.",
+		links:[
+			["itch","https://seansleblanc.itch.io/tackle-tourney-turbo"],
+			["git","https://github.com/seleb/FishingJam2016"]
+		]
+	},
+	{
+		title: "listen to my game idea",
+		image: "listen-to-my-game-idea",
+		association: "#gameideajam",
+		tags:["software","PICO-8"],
+		date: "October 2016",
+		tagline: "it's gonna be great",
+		description: "PICO-8 elevator pitches of the worst variety.",
+		links:[
+			["itch","https://seansleblanc.itch.io/listen-to-my-game-idea"],
+			["genericlink","http://www.lexaloffle.com/bbs/?tid=27909"]
+		]
+	},
+	{
+		title: "push the button to win!",
+		image: "push-the-button-to-win",
+		association: "#gameideajam",
+		tags:["software","games","PixiJS"],
+		date: "October 2016",
+		tagline: "it's easy",
+		description: "A push the button to win game. Made with Pixi.js.",
+		links:[
+			["itch","https://seansleblanc.itch.io/push-the-button-to-win"],
+			["git","https://github.com/seleb/GAMEIDEAJAM2016"]
+		]
+	},
+	{
+		title: "Rumble Road",
+		image: "Rumble Road",
+		association: "#GBJAM 5",
+		tags:["software","games","PixiJS"],
+		date: "October 2016",
+		tagline: "Turn-based GameBoy Combat",
+		description: "A short adventure inspired by JRPG classics. Made with Pixi.js.",
+		links:[
+			["itch","https://seansleblanc.itch.io/rumble-road"],
+			["git","https://github.com/seleb/GBJAM2016"]
+		]
+	},
+	{
+		title: "Duck Duck on the Loose",
+		image: "Duck Duck on the Loose",
+		association: "Personal Project",
+		tags:["software","games","PICO-8"],
+		date: "September 2016",
+		tagline: "Look, walk, and talk like a duck!",
+		description: "A pseudo-3D open-world game made with PICO-8. It has ducks!",
+		links:[
+			["itch","https://seansleblanc.itch.io/duck-duck-on-the-loose"],
+			["git","https://github.com/seleb/DuckDuckOnTheLoose"],
+			["genericlink","http://www.lexaloffle.com/bbs/?tid=27702"]
+		]
+	},
+	{
+		title: "In Transit",
+		image: "In Transit",
+		association: "Ludum Dare 26",
+		tags:["software","games","PixiJS"],
+		date: "September 2016",
+		tagline: "Relax and watch a probe search for life.",
+		description: "Not really a game, but relaxing to watch. Made with Pixi.js.",
+		links:[
+			["itch","https://seansleblanc.itch.io/in-transit"],
+			["git","https://github.com/seleb/FermiJam2016"]
+		]
+	},
+	{
+		title: "STARSHIP",
+		image: "STARSHIP",
+		association: "Carleton University",
+		tags:["software","games"],
+		date: "September 2016",
+		tagline: "A game about picking up stars with a spaceship!",
+		description: "A simple game made in Processing. (originally a school project, updated for online release).",
+		links:[
+			["itch","https://seansleblanc.itch.io/starship"],
+			["git","https://github.com/seleb/STARSHIP"]
+		]
+	},
+	{
+		title: "DYSCOVR",
+		image: "DYSCOVR",
+		association: "Ludum Dare 36",
+		tags:["software","games","PixiJS"],
+		date: "August 2016",
+		tagline: "the open source platform for intergalactic archaeology",
+		description: "An asynchronous online multiplayer sandbox about describing ancient alien technology. Made with Pixi.js.",
+		links:[
+			["itch","https://sweetheartsquad.itch.io/DYSCOVR"],
+			["git","https://github.com/seleb/LudumDare36"],
+			["genericlink", "http://ludumdare.com/compo/ludum-dare-36/?action=preview&uid=50235"]
+		]
+	},
+	{
+		title: "One Button Duel",
+		image: "One Button Duel",
+		association: "#OneButtonJam",
+		tags:["software","games","PICO-8"],
+		date: "August 2016",
+		tagline: "A one button dueling game.",
+		description: "This is a simple dueling game made with PICO-8. A basic peer-to-peer multiplayer mode is included through Peerjs.",
+		links:[
+			["itch","https://seansleblanc.itch.io/p8t"],
+			["git","https://github.com/seleb/pico-8-gpio"]
+		]
+	},
+	{
+		title: "P8T",
+		image: "P8T",
+		association: "Personal Project",
+		tags:["software","tool","PICO-8"],
+		date: "July 2016",
+		tagline: "A PICO-8 Twitter Client",
+		description: "A simple Twitter client made with PICO-8. This is a proof-of-concept meant to demonstrate netcode capabilities.",
+		links:[
+			["itch","https://seansleblanc.itch.io/p8t"],
+			["git","https://github.com/seleb/pico-8-gpio"],
+			["genericlink","http://seans.site/stuff/P8T"]
+		]
+	},
+	{
+		title: "PICO-8 Styler",
+		image: "PICO-8 Styler",
+		association: "Personal Project",
+		tags:["software","tool"],
+		date: "July 2016",
+		tagline: "Pretty PICO-8 Pages!",
+		description: "A PHP tool for generating simple customized pages for PICO-8 games.",
+		links:[
+			["itch","https://seansleblanc.itch.io/pico-8-styler"],
+			["git","https://github.com/seleb/PICO-8-Styler"],
+			["genericlink","http://seans.site/stuff/PICO-8-Styler"]
+		]
+	},
+	{
+		title: "atopy",
+		image: "atopy",
+		association: "Personal Project",
+		tags:["software","games","PICO-8"],
+		date: "June 2016",
+		tagline: "try to fight it",
+		description: "A viscerally challenging and challengingly visceral stealth combat game.",
+		links: [
+			["itch","https://seansleblanc.itch.io/atopy"],
+			["git","https://github.com/seleb/atopy"]
+		]
+
+	},
+	{
+		title: "Soda-Pop Sailin'",
+		image: "Soda-Pop Sailin",
+		association: "#simplejam",
+		tags:["software","games","PICO-8"],
+		date: "June 2016",
+		tagline: "Save your crew from the carbonated seas!",
+		description: "A zero-asset PICO-8 game. Everything including graphics and sound are generated through code.",
+		links: [
+			["itch","https://seansleblanc.itch.io/soda-pop-sailin"],
+			["git","https://github.com/seleb/SimpleJam"]
+		]
+	},
+	{
+		title: "Star Picker-Upper",
+		image: "Star Picker-Upper",
+		association: "#p8jam2",
+		tags:["software","games","PICO-8"],
+		date: "May 2016",
+		tagline: "A game about picking up stars!",
+		description: "A simple game made with PICO-8. Make sure to check out the cool palette swaps.",
+		links: [
+			["itch","https://seansleblanc.itch.io/star-picker-upper"],
+			["git","https://github.com/seleb/p8jam2"]
+		]
+
+	},
+	{
+		title: "Deluge Suites",
+		image: "Deluge Suites",
+		association: "#CITYSIMJAM",
+		tags:["software","games","S-Tengine2"],
+		date: "May 2016",
+		tagline: "Can you keep your hotel afloat?",
+		description: "A cheery little isometric game about managing a business during the end of the world.",
+		links: [
+			["itch","https://sweetheartsquad.itch.io/deluge-suites"],
+			["git","https://github.com/seleb/CITYSIMJAM"]
+		]
+	},
+	{
+		title: "Purity",
+		image: "Purity",
+		association: "Ludum Dare 35",
+		tags:["software","games","S-Tengine2"],
+		date: "April 2016",
+		tagline: "<strong>Protect</strong> your <strong>fragile</strong> heart from the <strong>infectious</strong> impurities!",
+		description: "A reverse bullet-hell game with a hyper-agressive aesthetic and a steep difficulty curve. Even I can't get past level 10!",
+		links: [
+			["itch","https://sweetheartsquad.itch.io/purity"],
+			["git","https://github.com/seleb/LD35"],
+			["genericlink","http://ludumdare.com/compo/ludum-dare-35/?action=preview&uid=50235"]
+		]
+	},
+	{
+		title: "SODZEN",
+		image: "SODZEN",
+		association: "#LOWREZJAM2016",
+		tags:["software","games","S-Tengine2"],
+		date: "April 2016",
+		tagline: "A little pot of paradise.",
+		description: "A 64 by 64 pixel zen experiment created for a game jam. See other entries I worked on <a target=\"_blank\" href=\"http://seans.site/stuff/LOWREZJAM2016\">here</a>.",
+		links: [
+			["itch","https://sweetheartsquad.itch.io/sodzen"],
+			["git","https://github.com/seleb/LOWREZJAM"],
+			["genericlink","http://seans.site/stuff/LOWREZJAM2016"]
+		]
+	},
+	{
+		title: "CODENAME: PAWN",
+		image: "CODENAME PAWN",
+		association: "Mini-Ludum Dare 66",
+		tags:["software","games","S-Tengine2"],
+		date: "March 2016",
+		tagline: "You have to kill the sleeper agents.",
+		description: "A surveillance-themed RTS-lite. This is a game of patience and attention to detail.",
+		links: [
+			["itch","https://sweetheartsquad.itch.io/codename-pawn"],
+			["git","https://github.com/seleb/MiniLD66"]
+		]
+	},
+	{
+		title: "Party, Darling?",
+		image: "Party Darling",
+		association: "Carleton University",
+		tags:["software","games","S-Tengine2"],
+		date: "July 2015 - April 2016",
+		tagline: "A weird &amp; abrasive sci-fi mystery adventure game.",
+		description: "A procedurally generated adventure game. This is my team's senior project from Carleton.",
+		links: [
+			["itch","https://sweetheartsquad.itch.io/party-darling"],
+			["git","https://github.com/SweetheartSquad/Game"]
+		]
+	},
+	{
+		title: "Go On",
+		image: "Go On",
+		association: "Ludum Dare 34",
+		tags:["software","games","S-Tengine2"],
+		date: "December 2015",
+		tagline: "A two-button game about growing up.",
+		description: "Originally just a lighting experiment, this game was inspired by <em>Passage</em> and is awfully moody.",
+		links: [
+			["itch","https://sweetheartsquad.itch.io/go-on"],
+			["git","https://github.com/seleb/LudumDare34"],
+			["genericlink","http://ludumdare.com/compo/ludum-dare-34/?action=preview&uid=50235"]
+		]
+	},
+	{
+		title: "StarBunny",
+		image: "StarBunny",
+		association: "Carleton University",
+		tags:["software","music","threejs"],
+		date: "October 2015",
+		tagline: "A WebGL audiovisual experience.",
+		description: "Features the Stanford bunny and music by Retroman. Built with Three.js and the AudioContext API.",
+		links: [
+			["genericlink","http://seans.site/stuff/webgl-starbunny"]
+		]
+	},
+	{
+		title: "S-Tengine 2",
+		image: "S-Tengine-2",
+		association: "Carleton University",
+		tags:["software","games","tool","S-Tengine2"],
+		date: "July 2014 - Present",
+		tagline: "The best game engine ever made by people who don't know how to make game engines.",
+		description: "An open source OpenGL-based C++ game engine. Used to develop over a dozen indie freeware titles.",
+		links: [
+			["git","https://github.com/SweetheartSquad/S-Tengine2"]
+		]
+	},
+	{
+		title: "3dSplineConnections",
+		image: "Hair",
+		association: "University of Ottawa",
+		tags:["software","tool","research"],
+		date: "Fall 2014",
+		tagline: "Hair modelling research.",
+		description: "A Cinder-based 3D UI used to optimize data in a hair modelling pipeline.",
+		links: [
+		]
+	},
+	{
+		title: "SPH",
+		image: "SPH",
+		association: "Carleton University",
+		tags:["software","research"],
+		date: "Summer 2013",
+		tagline: "Smoothed Particle Hydrodynamics research.",
+		description: "Contributions to SPH system included plastic deformation model, optimizations, and Maya visualization tools.",
+		links: [
+		]
+	}
+];
---
+++ js/index.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+	var vars={};
+	var parts = window.location.search.substring(1).split("&");
+
+	for(var i=0; i < parts.length; ++i){
+		var v = parts[i].split("=");
+		if(v.length >= 2){
+			vars[v[0]] = v[1].toLowerCase();
+		}
+	}
+
+	var tags = vars["tag"] !== undefined && vars["tag"] !== "all";
+
+	if(tags){
+		document.getElementById("current-tag").innerHTML = vars["tag"];
+	}
+
+	$(".entries").html("");
+	for(entry in db){
+		entry = db[entry];
+
+		
+		// if a tag is defined, skip entries which don't have that tag
+		if(tags){
+			var include = false;
+			var tags=entry.tags;
+			for(var i=0;i<tags.length;++i){
+				if(tags[i].toLowerCase() == vars["tag"]){
+					include = true;
+					break;
+				}
+			}
+			if(!include){
+				continue;
+			}
+		}
+
+		var s = "<li class=\"entry\">";
+		s += "<img class=\"static\" src=\"assets/images/thumbnails/" + entry.image + ".png\" alt=\"" + entry.title + " - thumbnail\" width=\"100%\" height=\"100%\"/>";
+		s += "<img class=\"staticOverlay\" src=\"data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk2A8AAMUAwUPJ2C4AAAAASUVORK5CYII=\" width=\"100%\" height=\"100%\"/>";
+
+		s += "<img class=\"animated\" src=\"assets/images/thumbnails/" + entry.image + ".gif\" alt=\"" + entry.title + " - thumbnail\" width=\"100%\" height=\"100%\"/>";
+		s += "<span>" + entry.tagline + "</span>";
+		s += "<h3>" + entry.title + "</h3>";
+		s += "<h4>" + entry.association + "<br>" + entry.date + "</h4>";
+		s += "<p>" + entry.description + "</p>";
+		s += "<h4>tags: " + getTags(entry.tags) + "</h4>";
+		s += "<div class=\"links\">";
+		for(l in entry.links){
+			var link = entry.links[l];
+			s += "<a target=\"_blank\" href=\"" + link[1] + "\" class=\"icon "+link[0]+"mark\"";
+			if(link[1] == ""){
+				s+= "style='visibility:hidden;'"
+			}
+			s += "></a>";	
+		}
+		s += "<a target=\"_blank\" href=\"" + entry.gitsource + "\" class=\"icon gitmark\"";
+		if(!entry.gitsource || entry.gitsource == ""){
+			s+= "style='visibility:hidden;'"
+		}
+		s += "></a>";
+		s += "</div>";
+		s += "<hr>";
+		s += "</li>";
+		$(".entries").append(s);
+	}
+});
+
+
+function getTags(t){
+	var s="";
+	for(var i = 0; i < t.length; ++i){
+		s+="<a href=\"browse.html?tag="+t[i]+"\">"+t[i]+"</a>";
+		if(i != t.length - 1){
+			s+=", ";
+		}
+	}
+	return s;
+}
