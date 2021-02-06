4e3e7b3628026cc8b289e82532f4a95b73619b9c
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-02-05 21:21:00 -0500

    add favicon

---
+++ src/pages/_document.jsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon-32x32.png" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
