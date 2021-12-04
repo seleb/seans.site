af517d26ad933e00c807a260b6d6df4fbb055b11
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-12-03 20:22:11 -0500

    move favicon to app + remove custom document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head"
 import "normalize.css"
 import "../styles/_app.scss"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
