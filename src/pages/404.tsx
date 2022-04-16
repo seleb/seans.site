5d24d8854851e894a6114a4442a24fa96be4bacd
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-04-16 17:50:04 -0400

    slightly better 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link"
 import SEO from "../components/seo"
 
 export default function NotFound() {
   return (
     <>
       <SEO title="404" description="Not found" />
-      <h1>404</h1>
-      <h2>Not found</h2>
+      <main className="index">
+        <header className="bio">
+          <h1>404</h1>
+          <h2>Not found</h2>
+          <p>
+            <Link href="/">
+              <a>back to home</a>
+            </Link>
+          </p>
+        </header>
+      </main>
     </>
   )
 }
