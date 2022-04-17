6dfcc81ee7fea8a421ef16e7c5afc310a4e3c414
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-04-17 01:55:01 -0400

    add locale

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-	trailingSlash: true,
-	generateBuildId: () => 'bid',
-};
+  trailingSlash: true,
+  generateBuildId: () => "bid",
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+}
