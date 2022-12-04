0b2d8a5e17a9e51cdf9cbe863ed04eb1d20862c1
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-12-04 12:53:53 -0500

    fix port on local

--- src/content/url.ts
+++ src/content/url.ts
@@ -1,3 +1,2 @@
 import pkg from "../../package.json";
-
-export const siteUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${pkg.name}`;
+export const siteUrl = process.env.NODE_ENV === "development" ? "http://localhost" : `https://${pkg.name}`;
